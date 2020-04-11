a077624d202d9b73b1c42ab81a07b4b310e88ebd
kiranFullStack <kiran.fullstack@outlook.com>
2020-04-11 23:59:15 +0530

    connect mongodb with mongoose

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
+
+mongoose
+  .connect("mongodb://localhost:27017/trialdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected🥁🥁🥁🥁🥁🥁🥁🥁"))
+  .catch((err) => console.log(err))
+
 const port = process.env.PORT || 5000
 app.get("/", (req, res) => {
   res.send("Hey from express!!")

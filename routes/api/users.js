2cb3078f878574b07460b1f1628ff16e574bc9d7
kiranFullStack <kiran.fullstack@outlook.com>
2020-04-12 02:46:58 +0530

    User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const router = express.Router()
+const User = require("../../models/User")
+const bcrypt = require("bcryptjs")
 
 // @route            GET api/users/test
 // @description      Tests Users Route
@@ -9,4 +11,31 @@ router.get("/test", (req, res) => {
   res.json({ message: "Users Works" })
 })
 
+// @route            GET api/users/register
+// @description      Register a User
+// @access           PUBLIC
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" })
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      })
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err
+          newUser.password = hash
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err))
+        })
+      })
+    }
+  })
+})
 module.exports = router

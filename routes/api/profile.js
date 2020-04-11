4a9584d5dec64f037d4b2afa6f28e8b62f335397
kiranFullStack <kiran.fullstack@outlook.com>
2020-04-12 02:16:29 +0530

    User model created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
-//
-// ─── O ──────────────────────────────────────────────────────────────────────────
-//
+// @route            GET api/profile/test
+// @description      Tests Profile Route
+// @access           PUBLIC
 
 router.get("/test", (req, res) => {
   res.json({ message: "Profile Works" })
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
-//
-// ─── O ──────────────────────────────────────────────────────────────────────────
-//
+// @route            GET api/users/test
+// @description      Tests Users Route
+// @access           PUBLIC
 
 router.get("/test", (req, res) => {
   res.json({ message: "Users Works" })
